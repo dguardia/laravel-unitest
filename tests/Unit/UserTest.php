92922481b75f2018f019b90244b1d3e226bedf25
David Guardia <david.guardia@gmail.com>
2019-05-01 22:10:34 -0400

    making the first User test

--- tests/Unit/UserTest.php
+++ tests/Unit/UserTest.php
@@ -3,20 +3,18 @@
 namespace Tests\Unit;
 
 use Tests\TestCase;
+use Illuminate\Foundation\Testing\WithFaker;
 use Illuminate\Foundation\Testing\RefreshDatabase;
 
-class ExampleTest extends TestCase
+class UserTest extends TestCase
 {
     /**
-     * A basic test example.
+     * A basic unit test example.
      *
      * @return void
      */
-    public function testBasicTest()
+    public function testExample()
     {
         $this->assertTrue(true);
-    }
-    public function testBasicTest2(){
-        $this->assertTrue(false);
     }
 }
