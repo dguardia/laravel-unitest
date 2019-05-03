41ae092089a0752238735a0cb81e0c61a6921d93
David Guardia <david.guardia@gmail.com>
2019-05-02 23:18:10 -0400

    adding posts

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -3,6 +3,8 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
+use App\User;
 
 class HomeController extends Controller
 {
@@ -23,6 +25,23 @@ class HomeController extends Controller
      */
     public function index()
     {
+
         return view('home');
+    }
+
+    public function profile()
+    {
+        //$user = Auth::user();
+
+        $user = User::find(2);
+        //$user->name = 'Steve Joe';
+        //$user->save();
+        //$user->delete();
+        $users = User::all();
+
+        dd($users->count());
+
+
+        return view('profile', ['users' => $users]);
     }
 }
